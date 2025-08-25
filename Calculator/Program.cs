94b121c546ddaaf17bc08a12bbb6a8a393b7c073
blatterf <fabian.blatter@edu.tbz.ch>
2025-08-25 14:12:51 +0200

    implement parser

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,6 +8,10 @@ namespace Calculator
         [STAThread]
         static void Main()
         {
+            Parser p = new Parser();
+        
+            p.Evaluate("1+(-sin(1)^6) + (0+1)");
+            //p.Evaluate("(1 + 1) * (0.5 + 0.5) + (0.5 - 0.5) * 2 * (1 / 3)");
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
